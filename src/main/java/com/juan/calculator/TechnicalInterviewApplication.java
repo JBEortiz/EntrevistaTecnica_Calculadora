9eff94559d8e0832f14ad15b8334cfc864168ffb
juan <juan1992b.ortiz@gmail.com>
2022-07-11 22:44:49 +0200

    feat basic structure

---
+++ src/main/java/com/juan/calculator/TechnicalInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.juan.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juan/calculator/TechnicalInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
